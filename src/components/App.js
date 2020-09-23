d4e81638cca1a4e75a7c71faae4e4b993ee71979
lanastasov <lubomir.anastasov@gmail.com>
2020-09-23 18:51:30 +0300

    State with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import SampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
     state = {
@@ -29,6 +30,9 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
