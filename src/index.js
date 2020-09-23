10daec0f2aad2e6e1a0202ed38dc800d238dfa5b
lanastasov <lubomir.anastasov@gmail.com>
2020-09-23 12:52:44 +0300

    Routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './css/style.css';
 import App from './components/App';
 
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={StorePicker} />
+                <Route path="/store/:storeId" component={App} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
 
-render(<App />, document.querySelector('#main'));
+render(<Root />, document.querySelector('#main'));
