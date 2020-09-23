ee7cb6ac5d7af702fc8080c3aae4153c7cc50430
lanastasov <lubomir.anastasov@gmail.com>
2020-09-23 13:46:59 +0300

    from event and this value bind or constructor

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the url');
+        // first grab the text from the box
+        console.log(this.storeInput.value);
+        // second we are going to transition from / to /store/:storeId
+        
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name"
-                        defaultValue={getFunName()} />
+                        defaultValue={getFunName()}
+                        ref={(input)=>{this.storeInput = input}} />
                 <button type="submit">Visit Store -&gt;</button>
             </form>
 
